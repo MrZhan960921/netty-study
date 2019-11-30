f9c3f94163ec2df8d3913464a4141513045a35d1
zcq <zcq@zcqdeMacBook-Pro.local>
2019-11-30 15:25:39 +0800

    websocket netty

---
+++ hello-netty/src/main/java/com/zcq/websocket/WSServer.java
@@ -0,0 +1,33 @@
+package com.zcq.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author Maybeeeee
+ * @date 2019/11/30 1:59 下午
+ */
+public class WSServer {
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup mainGroup = new NioEventLoopGroup();
+        EventLoopGroup subGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap server = new ServerBootstrap();
+            server.group(mainGroup, subGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(null);
+
+            ChannelFuture future = server.bind(8088).sync();
+
+            future.channel().closeFuture().sync();
+        } finally {
+            mainGroup.shutdownGracefully();
+            subGroup.shutdownGracefully();
+        }
+    }
+}
