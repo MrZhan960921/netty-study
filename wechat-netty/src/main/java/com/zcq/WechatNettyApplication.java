6b23df03ccbae062117da1d47b1bc00ace6ba519
zcq <zcq@zcqdeMacBook-Pro.local>
2019-12-11 21:31:24 +0800

    整合fastdfs

--- wechat-netty/src/main/java/com/zcq/WechatNettyApplication.java
+++ wechat-netty/src/main/java/com/zcq/WechatNettyApplication.java
@@ -9,7 +9,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 // 扫描mybatis mapper包路径
 @MapperScan(basePackages="com.zcq.mapper")
 // 扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
-@ComponentScan(basePackages= {"com.zcq"})
+@ComponentScan(basePackages= {"com.zcq","org.n3r.idworker"})
 public class WechatNettyApplication {
 
     public static void main(String[] args) {
--- wechat-netty/src/main/java/com/zcq/service/impl/UserServiceImpl.java
+++ wechat-netty/src/main/java/com/zcq/service/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.zcq.service.impl;
 
 
+import com.zcq.mapper.UsersMapper;
+import com.zcq.pojo.Users;
 import com.zcq.service.UserService;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -19,5 +22,63 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private UsersMapper userMapper;
 
+    @Autowired
+    private Sid sid;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public boolean queryUsernameIsExist(String username) {
+
+        Users user = new Users();
+        user.setUsername(username);
+
+        Users result = userMapper.selectOne(user);
+
+        return result != null ? true : false;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Users queryUserForLogin(String username, String pwd) {
+
+        Example userExample = new Example(Users.class);
+        Criteria criteria = userExample.createCriteria();
+
+        criteria.andEqualTo("username", username);
+        criteria.andEqualTo("password", pwd);
+
+        Users result = userMapper.selectOneByExample(userExample);
+
+        return result;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users saveUser(Users user) {
+
+        String userId = sid.nextShort();
+
+        // 为每个用户生成一个唯一的二维码
+        user.setQrcode("");
+
+        user.setId(userId);
+        userMapper.insert(user);
+
+        return user;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users updateUserInfo(Users user) {
+        userMapper.updateByPrimaryKeySelective(user);
+        return queryUserById(user.getId());
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    private Users queryUserById(String userId) {
+        return userMapper.selectByPrimaryKey(userId);
+    }
 }
